fd047abb06412b625da674f29d858d73a7d174f3
NicolasAmigo <namigo@fi.uba.ar>
2023-06-13 18:40:10 -0300

    Update RecursosApplication.java

--- src/main/java/com/squad12/recursos/RecursosApplication.java
+++ src/main/java/com/squad12/recursos/RecursosApplication.java
@@ -3,7 +3,9 @@ package com.squad12.recursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RecursosApplication {
 
@@ -11,7 +13,10 @@ public class RecursosApplication {
 		SpringApplication.run(RecursosApplication.class, args);
 	}
 
-
+	@GetMapping("")
+	public String main() {
+		return "Hola Mundo!";
+	}
 	@GetMapping("/recursos")
 	public String getRecursos() {
 		return "Recursos";
