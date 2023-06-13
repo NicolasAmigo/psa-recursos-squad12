5a9a4bfe7b34f417db3024e3f9409f9e8369a8f7
NicolasAmigo <namigo@fi.uba.ar>
2023-06-13 17:42:28 -0300

    Add recursos endpoint

--- src/main/java/com/squad12/recursos/RecursosApplication.java
+++ src/main/java/com/squad12/recursos/RecursosApplication.java
@@ -2,12 +2,19 @@ package com.squad12.recursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class RecursosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecursosApplication.class, args);
+	}
+
+
+	@GetMapping("/recursos")
+	public String getRecursos() {
+		return "Recursos";
 	}
 
 }
