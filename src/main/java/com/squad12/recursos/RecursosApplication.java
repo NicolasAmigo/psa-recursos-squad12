a8ccdfa030baaf4661ce743d01cd2918c002a9f0
NicolasAmigo <namigo@fi.uba.ar>
2023-06-14 14:19:17 -0300

    Add redirect from external api

--- src/main/java/com/squad12/recursos/RecursosApplication.java
+++ src/main/java/com/squad12/recursos/RecursosApplication.java
@@ -5,21 +5,47 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 @RestController
 @SpringBootApplication
 public class RecursosApplication {
+
+	String api = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.1/m/api/recursos";
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecursosApplication.class, args);
 	}
 
-	@GetMapping("")
+	@GetMapping("/")
 	public String main() {
 		return "Hola Mundo!";
 	}
 	@GetMapping("/recursos")
-	public String getRecursos() {
-		return "Recursos";
+	public String getRecursos() throws java.io.IOException{
+
+
+		URL url = new URL(api);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		int status = con.getResponseCode();
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+		in.close();
+		con.disconnect();
+		System.out.println(content);
+		return content.toString();
+
 	}
 
 }
