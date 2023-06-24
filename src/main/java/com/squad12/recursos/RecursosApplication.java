0608c696db95a74c9337fb8576e114c37c1b2042
NicolasAmigo <namigo@fi.uba.ar>
2023-06-24 17:30:36 -0300

    Added support for cors

--- src/main/java/com/squad12/recursos/RecursosApplication.java
+++ src/main/java/com/squad12/recursos/RecursosApplication.java
@@ -2,6 +2,7 @@ package com.squad12.recursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ public class RecursosApplication {
 		SpringApplication.run(RecursosApplication.class, args);
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/")
 	public String main() {
 		return "Hola Mundo!";
@@ -43,7 +45,6 @@ public class RecursosApplication {
 		}
 		in.close();
 		con.disconnect();
-		System.out.println(content);
 		return content.toString();
 
 	}
